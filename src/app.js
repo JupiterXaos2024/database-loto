99f435dde5a3c7a3da984c93c138b4749ddc8297
JupiterXaos2024 <149838940+JupiterXaos2024@users.noreply.github.com>
2024-07-13 19:38:59 -0500

    agregamos el * para la conexion

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const morgan = require('morgan')
 const config = require('./config.js')
-const app = express()
 const participantes = require('./moduls/data/participantes.js')
 const error = require('./red/errors.js')
+const http = require('http');
+const socketIo = require('socket.io');
 
+const app = express()
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*", // Cambia esto al dominio permitido
+        methods: ["GET", "POST"]
+    }
+});
 //middleware
 app.use(morgan('dev')) 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 //configuracion
+
 app.set('port', config.app.port)
 
 //rutas
